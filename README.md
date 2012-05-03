cf2588120c93f701b72fe0cf38bb54bab6d10888
Alexey Prohorenko <ap@alexeypro.com>
2012-05-03 15:49:58 -0700

    initial checking of just dropwizard project; it is NOT an archetype yet; to be converted though :-)

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ dropwizard-archetype
 ====================
 
 Maven2 Archetype for Coda Hale&#39;s Dropwizard
+
+$ mvn package
+
+$ java -jar target/dropwizard-example-0.0.1-SNAPSHOT.jar server configs/hello-world.yml
+
+$ open http://localhost:8080/hello-world http://localhost:8080/hello-world?name=Successful+Dropwizard+User
+
+$ open http://localhost:8081/ http://localhost:8081/metrics http://localhost:8081/threads http://localhost:8081/healthcheck
+
+
---
+++ src/main/java/com/alexeypro/samples/services/HelloWorldService.java
@@ -0,0 +1,27 @@
+package com.alexeypro.samples.services;
+
+import com.alexeypro.samples.HelloWorldConfiguration;
+import com.alexeypro.samples.resources.HelloWorldResource;
+import com.alexeypro.samples.health.TemplateHealthCheck;
+import com.yammer.dropwizard.Service;
+import com.yammer.dropwizard.config.Environment;
+
+public class HelloWorldService extends Service<HelloWorldConfiguration> {
+    public static void main(String[] args) throws Exception {
+        new HelloWorldService().run(args);
+    }
+
+    private HelloWorldService() {
+        super("hello-world");
+    }
+
+    @Override
+    protected void initialize(HelloWorldConfiguration configuration,
+                              Environment environment) {
+        final String template = configuration.getTemplate();
+        final String defaultName = configuration.getDefaultName();
+        environment.addResource(new HelloWorldResource(template, defaultName));
+        environment.addHealthCheck(new TemplateHealthCheck(template));
+    }
+
+}
